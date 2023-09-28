7f802635b28857798a50828b9b14bb89c5d49ef3
Rashmi R S <rashmirangana.surpu@hcl.com>
2023-09-28 15:53:43 +0530

    Add project files.

---
+++ Example5/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Example5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Example5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Content("This is Orinal repository");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
